026570f9e403873b092ba0e0b982909f1c3c1c20
moongua404 <hogun.soongit@gmail.com>
2024-06-18 14:39:20 +0900

    cors trial 1

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { setupSwagger } from './util/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3000', // 프론트엔드 도메인 설정
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   setupSwagger(app);
 
   await app.listen(3000);
